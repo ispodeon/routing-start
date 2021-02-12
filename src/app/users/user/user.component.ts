738275bfc6895d9dd6fa2e790ac4a7e374b39108
Sandaime <ispodeon@yahoo.com>
2021-02-11 22:07:26 -0500

    using paramaterized routes and subscribing to params

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.user = {
+      id: this.route.snapshot.params['id'],   // if you know for sure that this component does not need to 
+      name: this.route.snapshot.params['name']// be reloaded from this same componet to update values then 
+    };                                        // you dont need to subscribe as show below
+    this.route.params
+    .subscribe(
+      (params: Params) => {
+        this.user.id = params['id'];
+        this.user.name = params['name'];
+      }
+    );
   }
 
 }
